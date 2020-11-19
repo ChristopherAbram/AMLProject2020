5c743fd3f5523f41393d181cb1e35d097a3af120
Mathias Bejlegaard Madsen <mathiasbejlegaard@hotmail.com>
2020-11-19 11:27:57 +0100

    introduced classes for methods and other stuff

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import numpy as np
+import abc
 import sys, os
+from datetime import datetime
 import tensorflow as tf
 from tensorflow.keras import datasets
 import logging
 logging.getLogger("tensorflow").setLevel(logging.INFO)
-from datetime import datetime
 
 class ReconstructionError(tf.keras.losses.Loss):
     def __init__(self):
@@ -16,6 +17,7 @@ class ReconstructionError(tf.keras.losses.Loss):
 
 class AutoencoderLayer(tf.keras.layers.Layer):
     def __init__(self, 
+    S, # reconstruction weights
     input_size, 
     nodes,
     **kwargs):
@@ -28,11 +30,13 @@ class AutoencoderLayer(tf.keras.layers.Layer):
             shape=[self.input_size, self.nodes],
             initializer="random_normal",
             trainable=True,
+            name="w"
         )
         self.b = self.add_weight(
             shape=[self.nodes], 
             initializer="random_normal", 
-            trainable=True
+            trainable=True,
+            name="b"
         )
 
     def call(self, x):
@@ -42,14 +46,15 @@ class AutoencoderLayer(tf.keras.layers.Layer):
 
 class Autoencoder(tf.keras.layers.Layer):
     def __init__(
-        self, 
+        self,
+        S, # reconstruction weights
         input_size,
         **kwargs):
         super(Autoencoder, self).__init__(name="autoencoder", **kwargs)
-        self.firstHiddenLayer = AutoencoderLayer(input_size, 200)
-        self.secondHiddenLayer = AutoencoderLayer(200, 100)
-        self.thirdHiddenLayer = AutoencoderLayer(100, 200)
-        self.outputLayer = AutoencoderLayer(200, input_size)
+        self.firstHiddenLayer = AutoencoderLayer(S, input_size, 200) # TODO: create separate encoder and decoder
+        self.secondHiddenLayer = AutoencoderLayer(S, 200, 100)
+        self.thirdHiddenLayer = AutoencoderLayer(S, 100, 200)
+        self.outputLayer = AutoencoderLayer(S, 200, input_size)
 
     def call(self, x):
         y = self.firstHiddenLayer(x)
@@ -63,17 +68,39 @@ class AutoencoderModel(tf.keras.Model):
     def __init__(
         self,
         input_size,
+        S, # reconstruction weights
         **kwargs):
         super(AutoencoderModel, self).__init__(name='autoencodermodel', **kwargs)
-        self.autoencoder = Autoencoder(input_size=input_size)
+        self.autoencoder = Autoencoder(S=S, input_size=input_size)
     
     def call(self, x):
         x_r = self.autoencoder(x)
         print("[INFO] returning autoencoder model result: ", x_r)
         return x_r
 
+
+class Methods(abc.ABC):
+    @abc.abstractmethod
+    def build_reconstruction_set(self, X):
+        """
+        Builds reconstruction set per each element in input X.
+        Returns reconstruction set and the set of weights for x_i
+        """
+        pass
+
+
+class PCA(Methods):
+    def __init__(self):
+        pass
+
+    def build_reconstruction_set(self, X):
+        S = np.ones(X.shape[0])
+        return X, S
+
+
 def main(argc, argv):
-    n_epochs = 1000
+    load_model = True
+    n_epochs = 10
     n_classes = 10
     n_samples = 1000 # reduce dataset
     max_val = 255
@@ -101,20 +128,40 @@ def main(argc, argv):
     
     input_size = X_train.shape[1]
 
+    method = PCA()
+    omega, S = method.build_reconstruction_set(X_train)
+
     # Categorical representation of the labels:
-    #y_train, y_test = tf.keras.utils.to_categorical(y_train, num_classes=n_classes), \
-    #    tf.keras.utils.to_categorical(y_test, num_classes=n_classes)
+    Y_train, y_test = tf.keras.utils.to_categorical(y_train[:n_samples], num_classes=n_classes), \
+        tf.keras.utils.to_categorical(y_test[:n_samples], num_classes=n_classes)
 
-    autoencoder = AutoencoderModel(input_size)
-
+    # Build model, show summary:
+    autoencoder = AutoencoderModel(input_size, S)
     optimizer = tf.keras.optimizers.SGD(learning_rate=1e-3)
     autoencoder.compile(optimizer, loss=ReconstructionError())
-    autoencoder.fit(X_train, 
-    X_train, # TODO: X_train should be y_train
-    verbose=1,
-    callbacks=[tensorboard_callback, autoencoder_images_callback],
-    epochs=n_epochs, 
-    batch_size=n_samples) 
+    autoencoder.build(X_train.shape) # have to pass X_train.shape, but not needed
+
+    autoencoder.summary()
+    tf.keras.utils.plot_model(autoencoder, 'img/dgae_pca.png')
+
+    path = '.weight/pca.h5'
+    if os.path.exists(path) and load_model:
+        autoencoder.load_weights(path)
+
+    else:
+        autoencoder.fit(X_train, 
+            omega,
+            verbose=1,
+            callbacks=[tensorboard_callback, autoencoder_images_callback],
+            epochs=n_epochs, 
+            batch_size=n_samples)
+        
+        autoencoder.save_weights(path, overwrite=True)
+
+    # Use the model to output images:
+    X_reconstructed = autoencoder.predict(X_test[:10])
+
+
 
     print("Done")
     return 0
